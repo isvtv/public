7c1551ac618f82572649905d8f0d990db6a89703
isvtv <79759943+isvtv@users.noreply.github.com>
2021-03-23 10:51:53 +0530

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,17 @@
+<?php
+$option = 'url';
+$op = $pdo->prepare("SELECT option_value FROM op WHERE option_name = ?");
+$op->execute([$option]);
+$opp = $op->fetch(PDO::FETCH_ASSOC);
+echo $opp['option_value'];
+?>
+
+
+<?php function get_option($option)
+{
+    $op = $pdo->prepare("SELECT option_value FROM op WHERE option_name = ?");
+    $op->execute([$option]);
+    $opp = $op->fetch(PDO::FETCH_ASSOC);
+    return $opp['option_value'];
+} ?>
+<?php echo get_option('url'); ?>
